c285ca8761fda6f6fc360e608c4ce703fb1e56c4
jmrigali <jmrigali@ymail.com>
2017-05-23 17:02:29 -0600

    handle out of bound index

--- pets.js
+++ pets.js
@@ -5,23 +5,23 @@ const fs = require('fs');
 var node = process.argv[0];
 var file = process.argv[1];
 var cmd = process.argv[2];
+var index = process.argv[3];
 
 if (cmd === 'read') {
-  fs.readFile(guestsPath, 'utf8', function(err, data) {
+  fs.readFile('./pets.json', 'utf8', function(err, data) {
     if (err) {
       throw err;
     }
 
-    var guests = JSON.parse(data);
-
-    console.log(guests);
+    var pets = JSON.parse(data);
+      if(index > pets.length - 1) {
+        console.error(`Usage: ${node} ${file} read ${index}`);
+        process.exit(2);
+      }
+    console.log(pets[index]);
   });
 }
 else {
-//   console.error('error #%d', code);
-//   // Prints: error #5, to stderr
-//   console.error('error', code);
-//   // Prints: error 5, to stderr
   console.error(`Usage: ${node} ${file} read`);
   process.exit(1);
 }
