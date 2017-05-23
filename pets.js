2adcc0d175e442a96c1b433e25d3fd671867e99b
Shannon Rivers <shannon.rivers@gmail.com>
2017-05-23 17:34:45 -0600

    handle create subcommand

--- pets.js
+++ pets.js
@@ -5,27 +5,53 @@ const fs = require('fs');
 var node = process.argv[0];
 var file = process.argv[1];
 var cmd = process.argv[2];
-var index = process.argv[3];
+
+
 
 if (cmd === 'read') {
+  var petIndex = process.argv[3];
   fs.readFile('./pets.json', 'utf8', function(err, data) {
     if (err) {
       throw err;
     }
-
     var pets = JSON.parse(data);
-      if(index > pets.length - 1) {
+      if(petIndex > pets.length - 1) {
         console.error(`Usage: ${node} ${file} read ${index}`);
         process.exit(2);
       }
-    console.log(pets[index]);
+    console.log(pets[petIndex]);
+  });
+} else if (cmd === 'create'){
+  var age = process.argv[3];
+  var kind = process.argv[4];
+  var name = process.argv[5];
+  fs.readFile('./pets.json', 'utf8', function(readErr, data) {
+    if (readErr) {
+      throw readErr;
+    }
+    var pets = JSON.parse(data);
+      if (!age || !kind || !name){
+        console.error(`Usage: ${node} ${file} create AGE KIND NAME`);
+        process.exit(3);
+      }
+      var newPet = {};
+      newPet.age = age;
+      newPet.kind = kind;
+      newPet.name = name;
+
+      pets.push(newPet);
+
+      var petsJSON = JSON.stringify(pets);
+
+      fs.writeFile('./pets.json', petsJSON, function (writeErr){
+        if (writeErr){
+          throw writeErr;
+        }
+        console.log(newPet);
+      });
   });
 }
 else {
   console.error(`Usage: ${node} ${file} read`);
   process.exit(1);
 }
-
-
-
-// console.log(data);
